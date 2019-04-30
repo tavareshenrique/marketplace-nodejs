dc902bc489885c891ae9ebcd4546ddb1985b93f3
Henrique Tavares <ihenrits@gmail.com>
2019-04-29 23:30:39 -0300

    Template de e-mail

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,7 +13,8 @@ class PurchaseController {
       from: '"Henrique Tavares" <ihenrits@gmail.com>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
