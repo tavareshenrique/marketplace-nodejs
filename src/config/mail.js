76453c013f7bb64ffd58e485ca7c0f43f20d47a8
Henrique Tavares <ihenrits@gmail.com>
2019-04-30 01:24:16 -0300

    Váriaveis Ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '9c69ac7de109d8',
-    pass: '2c73400f3a7d0d'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://26b349e5171f4d2788b665476bb559ff@sentry.io/1449291'
+  dsn: process.env.SENTRY_DSN
 }
