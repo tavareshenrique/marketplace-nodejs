3ba742bfbceb6633883bcd369ddb790549ab61a9
Henrique Tavares <ihenrits@gmail.com>
2019-04-29 22:21:35 -0300

    Paginação

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -3,7 +3,15 @@ const Ad = require('../models/Ad')
 class AdController {
   // Método para Listagem
   async index (req, res) {
-    const ads = await Ad.find()
+    const ads = await Ad.paginate(
+      {},
+      {
+        page: req.query.page || 1,
+        limit: 20,
+        populate: ['author'],
+        sort: '-createdAt'
+      }
+    )
 
     return res.json(ads)
   }
