17827ee88c94c4dde59ca420e91b3fad6ebc3dd2
Henrique Tavares <ihenrits@gmail.com>
2019-04-29 23:59:12 -0300

    Validação

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth.js')
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(authMiddleware)
 
@@ -16,13 +26,17 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /**
  * Purchases
  */
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
